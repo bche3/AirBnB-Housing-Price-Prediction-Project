d1aa6e3802e86b674094fd5de77a64164d9964bb
bche3 <brianche3@gmail.com>
2022-12-09 06:09:38 -0800

    Update

--- scripts/preprocessing.R
+++ scripts/preprocessing.R
@@ -1,9 +1,30 @@
 library(tidyverse)
 
-data <- read_csv("data/unprocessed/listings.csv") 
+data <- read_csv("data/unprocessed/listings.csv") %>%
 
 glimpse(data)
 
+data <-data %>%
+
+colSums(is.na(is.numeric(data[,nums])))
+
+glimpse(data)
+
+nums <- unlist(lapply(data, is.numeric), use.names = FALSE)  
+nums
+
+  
+which(is.na(data), arr.ind=TRUE)  
+  
+data %>%
+  filter(price==0)
+data
+
+test <- subset(data, price > 0)
+test
+glimpse(data)
+
 set.seed(2022)
-subset <- sample(data, 1500)
-glimpse(subse
+rand_df <- data[sample(nrow(data), size=10000), ]
+
+
