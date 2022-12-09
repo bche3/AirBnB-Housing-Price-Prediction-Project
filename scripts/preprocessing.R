879d265ca3345cceb9668c77d5c3a1b5fe137015
bche3 <brianche3@gmail.com>
2022-12-09 07:38:41 -0800

    Update

--- scripts/preprocessing.R
+++ scripts/preprocessing.R
@@ -1,28 +1,25 @@
 library(tidyverse)
 
 data <- read_csv("data/unprocessed/listings.csv") %>%
+  # remove the 14 rows with price equal to 0 because a listing price of 0 can't be possible
+  filter(price != 0) %>%
+  filter(availability_365 != 0) %>%
+  select(-id, -name, -host_id, -host_name, -last_review, -license)
 
-glimpse(data)
+data <- read_csv("data/unprocessed/listings.csv") %>%
+  # remove the 14 rows with price equal to 0 because a listing price of 0 can't be possible
+  filter(price != 0) %>%
+  filter(availability_365 != 0) %>%
+  select(-id, -name, -host_id, -host_name, -last_review, -license, -neighbourhood, -latitude, -longitude, -number_of_reviews_ltm, -reviews_per_month)
 
-data <-data %>%
-
-colSums(is.na(is.numeric(data[,nums])))
-
-glimpse(data)
-
-nums <- unlist(lapply(data, is.numeric), use.names = FALSE)  
-nums
-
-  
-which(is.na(data), arr.ind=TRUE)  
-  
-data %>%
-  filter(price==0)
 data
 
-test <- subset(data, price > 0)
-test
+# AFTER EDA
+# drop neighbourhood, latitude, longitude,
+# drop number_of_reviews_ltm and reviews_per_month for being highly correlated with number_of_reviews
+data
 glimpse(data)
+
 
 set.seed(2022)
 rand_df <- data[sample(nrow(data), size=10000), ]
